5f0f1858870b5ce61377df03615c703ecec2eac8
Evan Hastings <56352068+evanrh@users.noreply.github.com>
2025-11-25 22:52:26 -0500

    feat(auth): finalized regitration and login endpoints
    - Adding new AuthService
    - Adding full business logic for registration
    - Adding better response marshalling for login endpoint

--- apps/server/src/routes/auth/index.ts
+++ apps/server/src/routes/auth/index.ts
@@ -1,54 +1,99 @@
 import { Hono } from "hono";
-import { UsersDAO } from "../../data-access/users-data-access.js";
 import { describeRoute, resolver, validator } from "hono-openapi";
-import { checkPasswordHash } from "../../lib/utils.js";
-import { authLoginResponseSchema, authLoginRequestSchema } from "./schemas.js";
-import { createAccessToken, createRefreshToken } from "../../lib/auth.js";
+import {
+  authLoginResponseSchema,
+  authLoginRequestSchema,
+  authRegistrationSchema,
+} from "./schemas.js";
+import { AuthService } from "../../services/auth-service.js";
+import {
+  errorResponseSchema,
+  genericSuccessResponseSchema,
+} from "../../lib/common-schemas.js";
 
 export const authRouter = new Hono();
-const usersDAO = new UsersDAO();
 
-const openApiTags = ['Auth'];
+const authService = new AuthService();
+
+const openApiTags = ["Auth"];
 
 authRouter
   .post(
-    '/login',
+    "/login",
     describeRoute({
-      description: 'Login user',
+      description: "Login user",
       tags: openApiTags,
       responses: {
         200: {
-          description: 'Successful response',
+          description: "Successful response",
           content: {
-            'application/json': { schema: resolver(authLoginResponseSchema) }
-          }
-        }
-      }
-    }),
-    validator('json', authLoginRequestSchema),
-    async (c) => {
-      const params = c.req.valid('json');
-      const user = await usersDAO.findUser(params);
-
-      if (user?.id) {
-        const passwordsMatch = await checkPasswordHash(params.password, user.password);
-        if (!passwordsMatch) {
-          return c.status(401)
-        }
-
-        const userId = user.id;
-        const [accessToken, refreshToken] = await Promise.all([
-          createAccessToken({ userId }),
-          createRefreshToken({ userId })
-        ]);
-        return c.json(
-          {
-            accessToken,
-            refreshToken,
+            "application/json": { schema: resolver(authLoginResponseSchema) },
           },
-          200
-        )
+        },
+        403: {
+          description: "Password mismatch",
+          content: {
+            "application/json": { schema: resolver(errorResponseSchema) },
+          },
+        },
+        404: {
+          description: "User not found",
+          content: {
+            "application/json": { schema: resolver(errorResponseSchema) },
+          },
+        },
+      },
+    }),
+    validator("json", authLoginRequestSchema),
+    async (c) => {
+      const { err, accessToken, refreshToken } = await authService.login(
+        c.req.valid("json"),
+      );
+
+      console.log(err);
+
+      if (err === "User not found") {
+        return c.json({ message: err }, 404);
+      } else if (err === "Invalid Password") {
+        return c.json({ message: err }, 403);
       }
-      return c.status(404)
-    }
+
+      return c.json(
+        { access_token: accessToken, refresh_token: refreshToken },
+        200,
+      );
+    },
   )
+  .post(
+    "/register",
+    describeRoute({
+      description: "Register for an account",
+      tags: openApiTags,
+      responses: {
+        200: {
+          description: "Successful response",
+          content: {
+            "application/json": {
+              schema: resolver(genericSuccessResponseSchema),
+            },
+          },
+        },
+        400: {
+          description: "Bad request",
+          content: {
+            "application/json": { schema: resolver(errorResponseSchema) },
+          },
+        },
+      },
+    }),
+    validator("json", authRegistrationSchema),
+    async (c) => {
+      const { err } = await authService.register(c.req.valid("json"));
+
+      if (typeof err === "string") {
+        return c.json({ message: err }, 400);
+      }
+
+      return c.json({ message: "success" }, 200);
+    },
+  );
--- apps/server/src/routes/auth/schemas.ts
+++ apps/server/src/routes/auth/schemas.ts
@@ -1,16 +1,27 @@
 import { z } from "zod";
 
-export const authLoginRequestSchema = z.object({
-  username: z.string().optional(),
-  email: z.string().optional(),
-  password: z.string()
-})
-.refine(
-  (obj) => obj.email || obj.username,
-  { message: 'An email or username is required' }
-)
+export const authLoginRequestSchema = z
+  .object({
+    username: z.string().optional(),
+    email: z.string().email().optional(),
+    password: z.string(),
+  })
+  .refine((obj) => obj.email || obj.username, {
+    message: "An email or username is required",
+  });
 
 export const authLoginResponseSchema = z.object({
-  accessToken: z.string(),
-  refreshToken: z.string(),
-})
+  access_token: z.string(),
+  refresh_token: z.string(),
+});
+
+export const authRegistrationSchema = z.object({
+  username: z.string().max(32),
+  email: z.string().email(),
+  password: z.string(),
+  inviteCode: z.string(),
+});
+
+export const authRegistrationResponseSchema = z.object({
+  message: z.string(),
+});
